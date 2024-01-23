60f9381c8fbeee130b333b090670cfcbb79c9e3c
sujin shin <shinsj5037@gmail.com>
2024-01-23 18:21:16 +0900

    띄어쓰기가 되지 않는 오류 수정

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -5,7 +5,7 @@ export default function InputTodo({setTotalTodos, lastId, setLastId}) {
     const [newTodo, setNewTodo] = useState('');
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        
         if (newTodo.length < 1) return;
         const newItem = {
             id: lastId,
@@ -18,7 +18,7 @@ export default function InputTodo({setTotalTodos, lastId, setLastId}) {
     }
     return (
         <form onSubmit={handleSubmit} className={styled.wrap}>
-            <input className={styled.input} placeholder='새로운 할 일' value={newTodo} onChange={(e)=>setNewTodo(e.target.value.trim())}></input>
+            <input className={styled.input} placeholder='새로운 할 일' value={newTodo} onChange={(e)=>setNewTodo(e.target.value.trimStart())}></input>
             <button className={styled.button}></button>
         </form>
     );
