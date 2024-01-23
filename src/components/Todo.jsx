25d6c0a2ae650991c196dc70753093943d1e11f7
sujin shin <shinsj5037@gmail.com>
2024-01-23 14:22:16 +0900

    전체 리스트 조회 기능

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaTrashAlt } from "react-icons/fa";
 import styled from './Todo.module.css'
 
-export default function Todo() {
+export default function Todo({data}) {
+    const { id, todo, done } = data;
+    const [checked, setChecked] = useState(false);
+    useEffect(() => {
+        setChecked(done);
+    }, [done]);
+    const handleChange = () => setChecked(prev => !prev);
     return (
         <div className={styled.wrap}>
-            <input className={styled.checkbox} id='todo' type="checkbox" />
-            <label className={styled.todo} htmlFor='todo'>공부하기</label>
+            <input className={styled.checkbox} id={`todo${id}`} type="checkbox" value={checked} onChange={handleChange} />
+            <label className={styled.todo} htmlFor={`todo${id}`}>{todo}</label>
             <button className={styled.button}><FaTrashAlt /></button>
         </div>
     );
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Todo from './Todo';
 import styled from './Todos.module.css'
 
 export default function Todos() {
+    const [todos, setTodos] = useState([]);
+    useEffect(() => {
+        fetch('data/todos.json')
+            .then(res => res.json())
+            .then(data => setTodos(data));
+    },[])
     return (
         <div className={styled.wrap}>
-            <Todo />
-            <Todo />
-            <Todo />
-            <Todo />
+            {
+                todos.map(todo=> <Todo key={todo.id} data={todo} />)
+            }
         </div>
     );
 }
