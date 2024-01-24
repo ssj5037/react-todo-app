456fa50aba31e977728cd605713ee11c5a82633a
sujin shin <shinsj5037@gmail.com>
2024-01-24 16:34:40 +0900

    불필요한 html 태그 정리, 주석 정리

--- src/App.jsx
+++ src/App.jsx
@@ -7,40 +7,34 @@ import { ThemeProvider } from './context/ThemeContext';
 
 export default function App() {
   const [filter, setFilter] = useState();
-  // const [darkTheme, setDarkTheme] = useState(localStorage.getItem('todo-darkTheme'));
-  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
+  // X : useState(getTodos()) -> 컴포넌트가 렌더링 될 때 마다 초기값 함수 매번 실행 (값은 useState가 내부적으로 가지고 있는 값을 사용한다.)
+  // O : useState(getTodos) -> 초기값이 필요할 경우에만 실행됨.
+  // 함수의 레퍼런스만 전달해 주기 때문에
+  const [todos, setTodos] = useState(getTodos);
   const filtered = getFilterTodos(todos, filter);
 
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
   
-  // useEffect(() => {
-  //   localStorage.setItem('todo-darkTheme', darkTheme);
-  // }, [darkTheme]);
-
-  // const handleTheme = () => setDarkTheme(prev => prev === 'dark' ? 'light' : 'dark');
-
   const handleInsert = (inserted) =>
     setTodos(prev => [...prev, inserted]);
-  
   const handleUpdate = (updated) =>
     setTodos(prev => prev.map(todo => todo.id === updated.id ? updated : todo));
-  
   const handleDelete = (deleted) =>
     setTodos(prev => prev.filter(todo => todo.id !== deleted.id));
 
   return (
     <ThemeProvider>
-      <div className='app'>
-        <div className='todo'>
-          <Header filter={ filter } onFilter={setFilter} />
-          <Todos todos={filtered} onUpdate={handleUpdate} onDelete={handleDelete} />
-          <InputTodo onInsert={handleInsert} />
-        </div>
-      </div>
+      <Header filter={ filter } onFilter={setFilter} />
+      <Todos todos={filtered} onUpdate={handleUpdate} onDelete={handleDelete} />
+      <InputTodo onInsert={handleInsert} />
     </ThemeProvider>
   );
+}
+
+function getTodos() {
+  return JSON.parse(localStorage.getItem('todos')) || []
 }
 
 function getFilterTodos(todos, filter) {
